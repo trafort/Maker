ed2be9a8911688e502e4d6a519d5a7b9e96f303d
trafort <34495933+trafort@users.noreply.github.com>
2018-02-23 19:29:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 const http = require('http')
-const port = 2000
+const port = 3000
 
 const requestHandler = (request, response) => {
   console.log(request.url)
-  response.end('Node!')
+  response.end('r u redy for node 25th?!')
 }
 
 const server = http.createServer(requestHandler)
@@ -14,5 +14,11 @@ server.listen(port, (err) => {
     return console.log('something bad happened', err)
   }
 
+ app.get('/', (request, response) => {
+  response.render('home', {
+   /// Replace "jhon" With ur name ////
+   name: 'John'
+  })
+}) 
   console.log(`server is listening on ${port}`)
 })
